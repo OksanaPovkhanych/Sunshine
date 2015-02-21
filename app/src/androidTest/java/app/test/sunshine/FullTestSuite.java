d8b0259caea229116ee1103d6b6c75d40adc0713
oksana <oknevskaya@gmail.com>
2015-02-21 17:17:08 +0200

    lesson 4a and 4b

---
+++ app/src/androidTest/java/app/test/sunshine/FullTestSuite.java
@@ -0,0 +1,21 @@
+package app.test.sunshine;
+
+/**
+ * Created by Ksyusha on 19.02.2015.
+ */
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
